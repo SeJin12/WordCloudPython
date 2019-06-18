ec5ba009bf72f5bc03873e072fd37422a80362a4
SeJin12 <tpwls8122@naver.com>
2019-06-18 18:46:34 +0900

    Link React with Flask

--- word_cloud.py
+++ word_cloud.py
@@ -1,4 +1,7 @@
-# 단어구름에 필요한 라이브러리를 불러옵니다.
+# # 단어구름에 필요한 라이브러리를 불러옵니다.
+# from urllib import request
+# from webbrowser import get
+
 from wordcloud import WordCloud
 # 한국어 자연어 처리 라이브러리를 불러옵니다.
 from konlpy.tag import Twitter
@@ -8,16 +11,21 @@ from collections import Counter
 import matplotlib.pyplot as plt
 # Flask 웹 서버 구축에 필요한 라이브러리를 불러옵니다.
 from flask import Flask, request, jsonify
+# 테스트를 위해 CORS를 처리
+from flask_cors import CORS
+# 파일에 접근하기 위한 라이브러리
+import os
 
 # 폰트 경로 설정
 font_path = 'NanumGothic.ttf'
 
 
 # 플라스크 웹 서버 객체 생성
-app = Flask(__name__)
+app = Flask(__name__, static_folder='outputs')
+CORS(app)
 
 
-def get_tage(text, max_count, min_length):
+def get_tags(text, max_count, min_length):
     t = Twitter()
     nouns = t.nouns(text)
     processed = [n for n in nouns if len(n) >= min_length]
@@ -39,20 +47,24 @@ def make_cloud_image(tags,file_name):
         background_color="white"
     )
     word_cloud = word_cloud.generate_from_frequencies(tags)
-    fig = plt.figure(figsize=(10,10))
+    fig = plt.figure(figsize=(10, 10))
     plt.imshow(word_cloud)
     plt.axis("off")
     # 만들어진 이미지 객체를 파일 형태로 저장
-    fig.savefig("outputs/{0}.png".format(file_name))
+    path = "outputs/{0}.png".format(file_name)
+    # 이미 파일 존재 시
+    if os.path.isfile(path):
+        os.remove(path)
+    fig.savefig(path)
 
 
-def process_from_text(text, max_count, min_length, words):
-    tags = get_tage(text, max_count, min_length)
+def process_from_text(text, max_count, min_length, words, file_name):
+    tags = get_tags(text, max_count, min_length)
     # 단어 가중치를 적용
     for n, c in words.items():
         if n in tags:
             tags[n] = tags[n] * int(words[n])
-    make_cloud_image(tags, "output")
+    make_cloud_image(tags, file_name)
 
 
 @app.route("/process", methods=['GET', 'POST'])
@@ -62,11 +74,30 @@ def process():
     if content['words'] is not None:
         for data in content['words'].values():
             words[data['word']] = data['weight']
-    process_from_text(content['text'], content['maxCount'], content['minLength'], words)
+    process_from_text(content['text'], content['maxCount'], content['minLength'], words, content['textID'])
     result = {'result': True}
     return jsonify(result)
 
 
+@app.route('/outputs', methods=['GET', 'POST'])
+def output():
+    text_id = request.args.get('textID')
+    return app.send_static_file(text_id+'.png')
+
+
+@app.route('/validate', methods=['GET', 'POST'])
+def validate():
+    text_id = request.args.get('textID')
+    path = "outputs/{0}.png".format(text_id)
+    result = {}
+    # 해당 이미지 파일 존재 확인
+    if os.path.isfile(path):
+        result['result'] = True
+    else:
+        result['result'] = False
+    return jsonify(result)
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+    app.run('0.0.0.0', port=5000, threaded=True)  # 처리 속도 향상을 위해 쓰레드 적용
 
